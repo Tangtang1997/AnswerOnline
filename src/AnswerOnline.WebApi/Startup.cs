55ce6cec99be062d8732437514e86ea15b1f5997
Tangtang1997 <46021085+Tangtang1997@users.noreply.github.com>
2022-02-17 14:03:10 +0800

    dfdsfsfsdf

--- src/AnswerOnline.WebApi/Startup.cs
+++ src/AnswerOnline.WebApi/Startup.cs
@@ -33,9 +33,9 @@ namespace AnswerOnline.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Log.Information("ConfigureServices");
-
             services.AddLogDashboard();
+
+            Log.Information("ConfigureServices");
 
             services.AddControllers();
 
