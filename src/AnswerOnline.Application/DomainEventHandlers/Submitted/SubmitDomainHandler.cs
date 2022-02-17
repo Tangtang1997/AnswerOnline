11ebb2b16a02481adb40f9aecb5e8c003760427a
Tangtang1997 <46021085+Tangtang1997@users.noreply.github.com>
2022-02-17 13:28:30 +0800

    update

--- src/AnswerOnline.Application/DomainEventHandlers/Submitted/SubmitDomainHandler.cs
+++ src/AnswerOnline.Application/DomainEventHandlers/Submitted/SubmitDomainHandler.cs
@@ -23,7 +23,7 @@ namespace AnswerOnline.Application.DomainEventHandlers.Submitted
             var participant = await _participantRepository.FirstOrDefaultAsync(x => x.Id == notification.SubmitterId, cancellationToken);
 
             //更新参赛者的成绩和答题用时
-            participant?.UpdateHighestScore(notification.Score,notification.TimeOfSubmit);
+            participant?.UpdateHighestScore(notification.Score, notification.TimeOfSubmit);
         }
     }
 }
